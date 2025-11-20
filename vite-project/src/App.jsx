f2496ede4dc3f9f6c686e1f1a8b4c58a738d612c
hanisha-senthilkumar <haniisenthil@gmail.com>
2025-11-20 21:36:52 +0530

    home.jsx Initial commit with basic Vite project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,51 +1,17 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-=======
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import {Home}  from "./Home"
+
+import { Home } from "./Home"
 import { NotFound } from "./NotFound"
 
->>>>>>> 13d4b5c (Upadate App.jsx file with new changes,index.css)
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-<<<<<<< HEAD
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-=======
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route index element={<Home/>} />
-        <Route path="*" element={<NotFound/>} />
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      </BrowserRouter>
->>>>>>> 13d4b5c (Upadate App.jsx file with new changes,index.css)
-    </>
+    </BrowserRouter>
   )
 }
 
-export default App
---
+++ vite-project/src/Home.jsx
@@ -0,0 +1,14 @@
+export  const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-forground overflow-x-hidden">
+            {/*Theme Toggle */}
+
+            {/*Background Effect */}
+            {/*Navbar*/}
+            {/*Main Content */}
+            {/*Footer*/}
+
+        </div>
+    )
+}
+
