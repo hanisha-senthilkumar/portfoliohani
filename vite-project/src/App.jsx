6a982826a669110f92f3f6788477b1a1641af26e
hanisha-senthilkumar <haniisenthil@gmail.com>
2025-11-20 19:26:48 +0530

    Upadate App.jsx file with new changes,index.css

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,21 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+=======
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {Home}  from "./Home"
+import { NotFound } from "./NotFound"
+
+>>>>>>> 13d4b5c (Upadate App.jsx file with new changes,index.css)
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+<<<<<<< HEAD
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +36,14 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+=======
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+      </BrowserRouter>
+>>>>>>> 13d4b5c (Upadate App.jsx file with new changes,index.css)
     </>
   )
 }
